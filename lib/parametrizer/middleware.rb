00d405745bcb5dfcfc9efc455c92b9d9561caa48
Adrian Serafin <abusiek@gmail.com>
2014-08-05 13:23:59 +0200

    only update country and language params if not defined

--- lib/parametrizer/middleware.rb
+++ lib/parametrizer/middleware.rb
@@ -11,8 +11,8 @@ module Parametrizer
       country = Parsers::CountryParser.new(env['HTTP_ACCEPT_LANGUAGE']).country
 
       request.update_param('oem', oem)
-      request.update_param('language', language)
-      request.update_param('country', country)
+      request.update_param('language', language) unless request.params.key?('language')
+      request.update_param('country', country) unless request.params.key?('country')
 
       @app.call(env)
     end
